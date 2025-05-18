d1168f2bc79bb228b902d3709e69cd9e08a34462
yutakasaito123 <13765586+yutakasaito123@users.noreply.github.com>
2025-05-18 02:17:44 +0000

    test

--- excel.py
+++ excel.py
@@ -2,4 +2,5 @@ import openpyxl
 from openpyxl.utils import get_column_letter,column_index_from_string
 wb = openpyxl.load_workbook('example.xlsx')
 sheet = wb.active
-print(tuple(sheet['a1':'c3']))
+for cell_obj in tuple(sheet.rows)[1]:
+    print(cell_obj.value)
